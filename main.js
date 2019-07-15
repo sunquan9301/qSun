3a49f6158247c892efeaa2298ae9276210a2c19d
sunquan <sunquan9301@163.com>
2019-07-15 15:54:29 +0800

    [Feature]add contract

---
+++ main.js
@@ -0,0 +1,4 @@
+
+function navigation(){
+    document.write('<ul><li><a href="index.html">Home</a></li><li><a href="research.html">Skills</a></li><li><a href="index.html">Hobbis</a></li><li><a href="index.html">Learn Plans</a></li><li><a href="index.html">Resources</a></li><li><a href="contract.html">Contract</a></li></ul>'); 
+}
