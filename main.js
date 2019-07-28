c76f57bee068cf5884ec892f710155ca099e4854
sunquan <sunquan9301@163.com>
2019-07-28 23:03:41 +0800

    [Feature]fix error

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 
 function navigation(){
-    document.write('<ul><li><a href="index.html">Home</a></li><li><a href="resource/resource.html">Resource</a></li><li><a href="contract.html">Contract</a></li></ul>'); 
+    document.write('<ul><li><a href="index.html">Home</a></li><li><a href="resource.html">Resource</a></li><li><a href="contract.html">Contract</a></li></ul>'); 
 }
